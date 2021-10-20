f9548b99d466d57a6f9590c4a6ec44a0087dbbdb
Albrecht Naumann <albrecht.naumann@posteo.de>
2021-10-20 21:42:41 +0200

    add vim

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@ BASEDIR=$(dirname $0)
 
 ZSHRC=$HOME/.zshrc 
 ZSHENV=$HOME/.zshenv 
+VIM=$HOME/.vim
 
 rm $ZSHRC
 rm $ZSHENV
+rm -rf $VIM
 
 ln -s $BASEDIR/zsh/zshrc $ZSHRC
 ln -s $BASEDIR/zsh/zshenv $ZSHENV
+ln -s $BASEDIR/vim $VIM
