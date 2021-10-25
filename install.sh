9dd1293c0b93a49083d80e0e43133281c0b26b7b
Albrecht Naumann <albrecht.naumann@zalari.de>
2021-10-25 16:47:07 +0200

    nvm update

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 
+which -s brew
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
+ 
+brew install nvm 
+
 ZSHRC=$HOME/.zshrc 
 ZSHENV=$HOME/.zshenv 
 VIM=$HOME/.vim
